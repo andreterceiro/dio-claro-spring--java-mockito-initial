84299d36d500916880fa25729d802838fd91aaac
André de Paula Terceiro <andreterceiro@yahoo.com.br>
2024-09-24 17:49:38 -0300

    Change in Java class of the test

--- artifactId/src/test/java/packageGroupId/AppTest.java
+++ artifactId/src/test/java/packageGroupId/AppTest.java
@@ -1,19 +1,28 @@
 package packageGroupId;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
+import java.util.List;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 /**
  * Unit test for simple App.
  */
+@ExtendWith(MockitoExtension.class)
 public class AppTest {
+
+    @Mock
+    private List<String> letras;
 
     /**
      * Rigorous Test :-)
      */
     @Test
     public void shouldAnswerWithTrue() {
-        assertTrue(true);
+        Mockito.when(letras.get(0)).thenReturn("Z");
+        Assertions.assertEquals("Z", letras.get(0));
     }
 }
